04136d93ac859d5f996e60a1fd292cff7f1cbbf8
SWATHIPRIYA16 <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-01-31 23:20:08 -0800

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -8,9 +8,15 @@ int main()
    {
     printf("Vowel");
    }
-  else
+  else if(ch == 'b'||ch == 'c'||ch == 'd'||ch == 'f'||ch == 'g'||ch == 'h'||ch == 'j'||ch == 'k'||ch == 'l'||ch == 'm'||ch == 'n'||ch == 'p'||ch == 'q'||ch == 'r'||ch == 's'||ch == 't'||ch == 'v'||
+  ch == 'w'||ch == 'x'||ch == 'y'||ch =='z')
     {
     printf("Consonant");
+    
+    }
+    else
+    {
+        printf("invalid");
     }
  
 }
