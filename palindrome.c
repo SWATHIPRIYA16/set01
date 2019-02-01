0df50290b63d2de6c452da5e0aa22fcfa091a129
SWATHIPRIYA16 <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-02-01 21:35:56 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 void main()
 {
-    int n, rev_Int = 0, remainder, ori_Int;
-    scanf("%d", &n);
-    ori_Int = n;
+    
+    long int n, rev_num= 0, remainder, ori_num;
+    scanf("%ld", &n);
+    ori_num = n;
+    if(ori_num>=1000)
+    {
     while( n!=0 )
     {
         remainder = n%10;
-        rev_Int = rev_Int*10 + remainder;
+        rev_num = rev_num*10 + remainder;
         n /= 10;
     }
-    if (ori_Int == rev_Int)
+    if (ori_num== rev_num)
         printf("yes");
     else
         printf("no");
-}
+}}
