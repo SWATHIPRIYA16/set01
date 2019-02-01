79d9e40bdc878f43571af4b4272b8383aeaecb7d
SWATHIPRIYA16 <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-01-31 23:58:57 -0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,14 @@
 # include <stdio.h>
 void main()
-{ int i,N;
+{
+    int i,N;
+     scanf("%d",&N);
  if(N>0)
  {
-     
-    scanf("%d",&N);
     for(i=0;i<=N;i++)
     {
-        printf("Hello"\n);
+        printf("Hello\n");
     }
  }
 }
+
