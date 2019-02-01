0c2e936746af2e918de3d399a6858b82c474502f
SWATHIPRIYA16 <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-02-01 21:38:02 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -5,8 +5,6 @@ void main()
     long int n, rev_num= 0, remainder, ori_num;
     scanf("%ld", &n);
     ori_num = n;
-    if(ori_num>=1000)
-    {
     while( n!=0 )
     {
         remainder = n%10;
@@ -17,4 +15,4 @@ void main()
         printf("yes");
     else
         printf("no");
-}}
+}
