8ca1fd761d4cb2a1c5f697f20dbd738d6a2027b9
SWATHIPRIYA16 <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-01-31 23:23:52 -0800

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+  char ch;
+  scanf("%c", &ch);
+  if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U'||ch == 'b'||ch == 'c'||ch == 'd'||ch == 'f'||ch == 'g'||ch == 'h'||ch == 'j'||ch == 'k'||ch == 'l'||ch == 'm'||ch == 'n'||ch == 'p'||ch == 'q'||ch == 'r'||ch == 's'||ch == 't'||ch == 'v'||
+  ch == 'w'||ch == 'x'||ch == 'y'||ch =='z')
+   {
+    printf("Alphabet");
+   }
+  else {
+        printf("No");
+    }
+ 
+}
